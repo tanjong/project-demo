af4a8ec56bf70d3f3943be5548d189b3a1f92095
Tanjong <tanjong.njie@outlook.com>
2022-05-28 09:22:02 -0500

    shell script tutorial

---
+++ myscript.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# ECHO COMMAND
+#echo Hello World!
+
+# VARIABLES
+#Uppercase by convention
+#Letters, numbers, underscres
+NAME="ArreyBob"
+# echo "My name is $NAME"
+#echo "My name is $NAME"
+
+#USER INPUT
+#read -p "Enter you name: " NAME
+#echo "Hello $NAME, nice to meet you!"
+
+# SIMPLE IF STATEMENTS
+#if [ "$NAME" == "Arrey" ]
+#then
+#   echo "Your name is Arrey"
+#fi   
+
+# IF-ELSE
+if [ "$NAME" == "Arrey" ]
+then
+   echo "Your name is Arrey"
+else
+   echo "Your name is NOT Arrey"
+fi   
